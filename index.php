3937178c86f7ef7d7f2725aa4ca670a655d648b8
joost <joost.c.kok@gmail.com>
2023-07-06 08:12:48 +0200

    static site

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
-echo '<h1> hello world </h1>';
-echo '<a href="info.php">info</a>';
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Page Title</title>
+</head>
+
+<body>
+    <header>
+        <h1> Hello World </h1>
+    </header>
+    <nav>
+        <fieldset>
+            <legend>Menu:</legend>
+            <table>
+                <tr>
+                    <td>
+                        <a href="info.php"> PHPInfo </a>
+                    </td>
+                </tr>
+            </table>
+        </fieldset>
+    </nav>
+    <article>This is my first hosted endevour :)</article>
+    <footer>joost kok 2023</footer>
+</body>
+
+</html>
